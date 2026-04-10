61bcfc67b802e0f1de58ab79ca79541840a2bc9f
Sakshi Suresh Chavan <sakshic2403@gmail.com>
2026-04-10 23:35:37 +0530

    removed plugin-react to fix render build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tailwindcss()],
   server: {
     proxy: {
-      "/api":{
-        target : "http://localhost:5000"
+      "/api": {
+        target: "http://localhost:5000"
       }
     }
   }
